538efc0fef83b8cbdb6db0668cbfdeb59743b447
katiesweet <katie.sweet@hotmail.com>
2016-11-30 19:27:13 -0700

    Linking to Server!

--- source/login.h
+++ source/login.h
@@ -1,28 +1,22 @@
 #ifndef LOGIN_H
 #define LOGIN_H
 
-#include <wx/wx.h>
-#include <wx/textctrl.h>
 #include <string>
+#include <wx/textctrl.h>
+#include <wx/wx.h>
 
-
-class login: public wxPanel
-{
+class login : public wxPanel {
 public:
-  login(wxFrame* parent);
-  void OnNewUser(wxCommandEvent & event);
-  void OnLogin(wxCommandEvent & event);
+  login(wxFrame *parent);
+  void OnNewUser(wxCommandEvent &event);
+  void OnLogin(wxCommandEvent &event);
+  void receivedLoginFromServer(std::string msg, wxCommandEvent &event);
   wxTextCtrl *usernameText;
   wxTextCtrl *passwordText;
   ~login();
 
-  enum{
-    loginButton = 2,
-    newUserButton = 3
-  };
+  enum { loginButton = 2, newUserButton = 3 };
   wxDECLARE_EVENT_TABLE();
 };
-
-
 
 #endif
