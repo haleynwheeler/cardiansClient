4dd38ae51f6528d0a503d1ea7210dd9290d4be38
MadCook <Madeline.Cook@aggiemail.usu.edu>
2016-12-04 00:35:05 -0700

    Fix 8's seg fault

--- source/GameLogic/CrazyEightsGame.cpp
+++ source/GameLogic/CrazyEightsGame.cpp
@@ -6,9 +6,8 @@
 
 using namespace std::chrono_literals;
 
-CrazyEightsGame::CrazyEightsGame(wxFrame *mainFrame, clientInfo *client)
-    : Game() {
-  gui = new playArea(mainFrame, client);
+CrazyEightsGame::CrazyEightsGame(wxFrame *mainFrame) : Game() {
+  gui = new playArea(mainFrame);
   gui->setMadeMoveFunction([this](Card c) { humanMadeMove(c); });
   gui->setDrewCardFunction([this]() { humanDrewCard(); });
   std::cout << "Created Play Area" << std::endl;
